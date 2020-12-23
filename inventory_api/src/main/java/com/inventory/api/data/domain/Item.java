8b599af14acaa3ea05ef6b329ce1488e3c78f3a0
MrWalshyType2 <mwalsh@academytrainee.com>
2020-12-23 12:39:21 +0000

    Added Item domain object. Closes #3

--- inventory_api/src/main/java/com/inventory/api/data/domain/Item.java
+++ inventory_api/src/main/java/com/inventory/api/data/domain/Item.java
@@ -1,43 +1,36 @@
-package com.inventory.api.data.document;
+package com.inventory.api.data.domain;
 
 import java.util.Set;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import com.inventory.api.data.models.ItemName;
 import com.inventory.api.data.models.Price;
 import com.inventory.api.data.models.Size;
 import com.inventory.api.data.models.Tag;
 
-import lombok.Data;
+import lombok.Value;
 
-@Data
-@Document
-public class ItemDocument {
+/**
+ * Immutable domain object to represent an Item from a DTO or Document access.
+ * 
+ * @author morgan
+ *
+ */
+@Value
+public class Item {
 
-	@MongoId
 	private String id;
 	
-	@Field
 	private ItemName name;
 	
-	@Field
 	private String description;
 	
-	@Field
 	private Price price;
 	
-	@Field
 	private Integer stock;
 	
-	@Field
 	private Size size;
 	
-	@Field
 	private Set<Tag> tags;
 	
-	@Field
 	private String itemCode;
 }
